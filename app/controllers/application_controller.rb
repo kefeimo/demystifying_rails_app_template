267326ea6b335baa4eca1b7c0285dceac4efc14f
Kefei Mo <kefeimo@gmail.com>
2022-12-05 21:16:13 -0600

    List and Show Records https://launchschool.com/books/demystifying_rails/read/list_and_show_records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,34 @@ class ApplicationController < ActionController::Base
     # render inline: '<em>Hello, World!</em>'
     # render "application/hello_world"
     # render inline: File.read("app/views/application/hello_world.html.erb")
-
     name = params["name"] || "world"
     p "p name #{name}"
     p name
     # name = "keessdfd"
     render "application/hello_world", locals: { name: name}
 
+  end
 
+  def list_posts
+
+
+    posts = connection.execute("SELECT * FROM posts")
+
+    render "application/list_posts", locals: {posts:posts}
+  end
+
+  def show_post
+
+    id = params[:id]
+    puts id
+    post = connection.execute("SELECT * FROM posts where id==#{id}").first
+    # render "application/list_posts", locals: {posts:posts}
+    render "application/show_post", locals: {post: post}
+  end
+  
+  def connection
+    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection.results_as_hash = true
+    db_connection
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # get({"/hello_world" => "application#hello_world"})
   get "/hello_world" => "application#hello_world"
   get "/hello_world/:name" => "application#hello_world"
+
+  get "/list_posts" => "application#list_posts"
+  get "/show_post/:id" => "application#show_post"
+
   # get 'welcome/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
